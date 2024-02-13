053a5f123afeda39aead00359941610e0c845bbb
Rhev <145690423+RhevMndz@users.noreply.github.com>
2024-02-14 01:05:57 +0800

    una

---
+++ app.js
@@ -0,0 +1,4 @@
+document.querySelector('.container').addEventListener('click', function() {
+    document.querySelector('.message').innerHTML = "I love you!";
+    document.querySelector('.message').style.color = "#ff4d6d";
+});
